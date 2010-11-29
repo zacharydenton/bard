2b114847afe6803a4cf5361bf25ad66e2da30205
Zach Denton <zacharydenton@gmail.com>
2010-11-29 10:15:42 -0500

    updated setup.py to reflect new name.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 '''
-Installer script for the pseudorandom module.
+Installer script for the bard module.
 '''
 
 from distutils.core import setup
 
 setup (
-    name = "pseudorandom",
+    name = "bard",
     version = "0.2",
     author = 'Zach Denton',
     author_email = 'zacharydenton@gmail.com',
     url = 'http://zacharydenton.com/',
-    description = "A module which generates pseudorandom text.",
-    py_modules = ['pseudorandom']
+    description = "A module which generates pseudorandom text. Inspired by the Bard from Asimov's 'Someday'.",
+    py_modules = ['bard']
 )
